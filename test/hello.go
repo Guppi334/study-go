34308fb8ae39d11aeb9abd5a80c3bab9dff2de07
Guppi334 <st0910na0401@gmail.com>
2020-07-08 15:39:51 +0900

    finish channel

--- test/hello.go
+++ test/hello.go
@@ -1,33 +1,30 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
-func goroutin(s string, wg *sync.WaitGroup) {
-
-	defer wg.Done()
-	for i := 0; i < 5; i++ {
-		time.Sleep(100 * time.Microsecond)
-		fmt.Println(s)
+func goroutine1(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
 	}
+	c <- sum
 }
 
-func normal(s string) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(100 * time.Microsecond)
-		fmt.Println(s)
+func goroutine2(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
 	}
+	c <- sum
 }
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go goroutin("World", &wg)
-	normal("Hello")
-	// time.Sleep(2000 * time.Microsecond)
-	wg.Wait()
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int)
+	go goroutine1(s, c)
+	go goroutine2(s, c)
+	x := <-c
+	fmt.Println(x)
+	y := <-c
+	fmt.Println(y)
 }
