f353b0d96126371e00d4c2cb2151f8ce70730edf
Guppi334 <st0910na0401@gmail.com>
2020-07-08 15:49:41 +0900

    finish channel-range

--- test/hello.go
+++ test/hello.go
@@ -6,27 +6,16 @@ func goroutine1(s []int, c chan int) {
 	sum := 0
 	for _, v := range s {
 		sum += v
+		c <- sum
 	}
-	c <- sum
-}
-
-func goroutine2(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	c <- sum
+	close(c)
 }
 
 func main() {
-	ch := make(chan int, 2)
-	ch <- 100
-	fmt.Println(len(ch))
-	ch <- 200
-	fmt.Println(len(ch))
-	close(ch)
-
-	for c := range ch {
-		fmt.Println(c)
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int, len(s))
+	go goroutine1(s, c)
+	for i := range c {
+		fmt.Println(i)
 	}
 }
