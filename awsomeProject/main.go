9d89d4d868a7fd71e11b883e81c655551b02f61b
Guppi334 <st0910na0401@gmail.com>
2020-07-09 10:30:37 +0900

    finish websocket

--- awsomeProject/main.go
+++ awsomeProject/main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/url"
 
-	"github.com/markcheno/go-quote"
-	"github.com/markcheno/go-talib"
+	"github.com/gorilla/websocket"
 )
 
+type JsonRPC2 struct {
+	Version string      `json:"jsonrpc"`
+	Method  string      `json:"method"`
+	Params  interface{} `json:"params"`
+	Result  interface{} `json:"result, omitempty"`
+	Id      *int        `json: "id, ommitempty"`
+}
+
+type SubscribeParams struct {
+	Channel string `json:"channel"`
+}
+
 func main() {
-	spy, _ := quote.NewQuoteFromYahoo("spy", "2019-01-01", "2020-04-01", quote.Daily, true)
-	fmt.Print(spy.CSV())
-	rsi2 := talib.Rsi(spy.Close, 2)
-	fmt.Println(rsi2)
-	mva := talib.Ema(spy.Close, 14)
-	fmt.Println(mva)
+	u := url.URL{Scheme: "wss", Host: "ws.lightstream.bitflyer.com", Path: "/json-rpc"}
+	log.Printf("connecting to %s\n", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatalln("dial", err)
+	}
+	defer c.Close()
+
+	if err := c.WriteJSON(&JsonRPC2{Version: "2.0", Method: "subscribe", Params: &SubscribeParams{"lightning_ticker_BTC_JPY"}}); err != nil {
+		log.Fatalln("dial:", err)
+		return
+	}
+
+	for {
+		message := new(JsonRPC2)
+		if err := c.ReadJSON(message); err != nil {
+			log.Fatalln("read:", err)
+		}
+
+		if message.Method == "channelMessage" {
+			log.Println(message.Params)
+		}
+	}
 }
